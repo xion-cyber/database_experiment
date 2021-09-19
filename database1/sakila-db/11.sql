f782e74392472e29ae7287d21d112a4e68d50dea
xion <9743974356215@qq.com>
2021-09-19 23:29:45 +0800

    hw1

--- database1/sakila-db/11.sql
+++ database1/sakila-db/11.sql
@@ -2,3 +2,4 @@ insert into address
 values(606,'1329 Fukuyama Street',null,'Heilongjiang',537,27107,288241215399,st_geomfromtext('POINT (128.0449753 46.9804391)'),now());
 insert into customer
 values(600,1,"Luis","Sword",'Sword.Luis@sakilacustomer.org',606,1,now(),now());
+
